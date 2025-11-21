5bf6c0d2a8e5c9307aca412a31c86fc47686f447
Marc B <75181479+Saysikerightnow@users.noreply.github.com>
2025-11-21 09:45:43 +0100

    Update TESTscrape.py

--- TESTscrape.py
+++ TESTscrape.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 response = requests.get("https://millercenter.org/the-presidency/presidential-speeches/april-30-1789-first-inaugural-address")
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Find the main content container
+# Fetch the desired content
 content_div = soup.find("div", class_="view-transcript")
 if content_div:
     for para in content_div.find_all("p"):
@@ -15,4 +15,3 @@ if content_div:
 else:
     print("No article content found.")   
     
-    Jeg er verden 
